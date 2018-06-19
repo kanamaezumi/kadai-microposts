359e29af6fefcad11c235246b9f1adb1d1793196
kana.maezumi <kana.a.maezumi@rakuten.com>
2018-06-19 01:13:40 +0000

    modification

--- routes/web.php
+++ routes/web.php
@@ -32,7 +32,7 @@ Route::group(['prefix' => 'users/{id}'], function () {
        Route::get('followings', 'UsersController@followings')->name('users.followings');
        Route::get('followers', 'UsersController@followers')->name('users.followers');
        Route::post('favorite', 'UserFavoriteController@store')->name('user.favorite');
-       Route::delete('unfavorite', 'UserFavoriteControlle@destroyr')->name('user.unfavorite');
+       Route::delete('unfavorite', 'UserFavoriteController@destroy')->name('user.unfavorite');
        Route::get('favoriter', 'UsersController@favoriter')->name('users.favoriter');
     });
 Route::resource('microposts', 'MicropostsController', ['only' => ['store', 'destroy']]);
